f34a703d16df0275f52de5a748a76332e4ffc6d0
Margaret Egner <hellogoodbyemae@gmail.com>
2020-05-09 08:42:16 -0400

    Pseudocode JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,16 @@
+// PSEUDOCODE: 
+// 1. Create START button to begin quiz.
+// 2. Begin 120 SECOND TIMER to answer the multiple choice questions.
+//      1. IF all questions are answered before time runs out, game ends.
+//      2. IF time runs out before all questions are answered, game ends.
+// 3. Display results.
+//      1. Win/Lose Results.
+//      2. Correct Answers.
+//      3. Incorrect Answers.
+//      4. Unanswered.
+
 $(document).ready(function() {
 
-    
+
 
 })
