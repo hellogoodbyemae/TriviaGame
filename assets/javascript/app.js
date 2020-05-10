87a0fb8e5d05b810f23f113496e3235224bce207
Margaret Egner <hellogoodbyemae@gmail.com>
2020-05-09 23:09:45 -0400

    update js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
 // PSEUDOCODE: 
 // 1. Create START button to begin quiz.
+//      1. When START is clicked, the game fills screen and START disappears.
 // 2. Begin 120 SECOND TIMER to answer the multiple choice questions.
 //      1. IF all questions are answered before time runs out, game ends.
 //      2. IF time runs out before all questions are answered, game ends.
+// 
 // 3. Display results.
 //      1. Win/Lose Results.
 //      2. Correct Answers.
@@ -11,6 +13,28 @@
 
 $(document).ready(function() {
 
+    var number = 120;
+    var timerId;
+    var correctCount = 0;
+    var incorrectCount = 0;
+    var unanswered = 0;
+
+    function run() {
+        intervalId = setIterval(decrement, 1000);
+    }
+
+    $('#start').on('click', function() {
+        
+    }
 
 
+    function stop() {
+        clearInterval(intervalId);
+    }
+
+    function show() {
+        $('#time').show();
+    }
+
+    
 })
